7c0a5e2806de002b04a89e50a29ddc0eb4186b62
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-28 17:05:34 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,16 @@ class SongsController < ApplicationController
   end
 
   def show
+    if params[:artist_id]
+      @artist = Artist.find_by(:id => params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else
+        @songs = @artist.songs
+      end
+      else
     @song = Song.find(params[:id])
+    end
   end
 
   def new
