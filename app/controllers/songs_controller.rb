facf16cc9508aa1e99a694b7423e54854373d8fe
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-28 17:13:22 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,23 @@
 class SongsController < ApplicationController
   def index
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+      redirect_to artists_path, alert: "Artist not found"
+    else
+      @songs = @artist.songs
+    end
+    else
     @songs = Song.all
+  end
   end
 
   def show
-    @song = Song.find(params[:id])
+    @song = Song.find_by(id: params[:id])
+    if @song.nil?
+      flash[:alert] = "Song not found."
+      redirect_to artist_songs_path(params[:artist_id])
+    end
   end
 
   def new
@@ -50,4 +63,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
